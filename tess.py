972874fb3044285f83477f1ff060355ae46fb3dd
Kevin <farthing.kw@gmail.com>
2018-08-22 16:07:02 -0500

    added explanations

--- tess.py
+++ tess.py
@@ -6,25 +6,25 @@ import cv2
 import os
 import numpy as np
 
-# # img = Image.open("celltest row0 col5.jpg")
-# img = PIL.ImageOps.invert(Image.open("celltest row2 col6.jpg"))
-# # img.show()
-
-# text = pytesseract.image_to_string(img, config="--psm 10")
-# print("celltest found :" + text +": and no more")
-# answers{'03':'1','05':'4','06':'8','21':'2','22':'1','26':'9','28':'7','33':'3','34':'1','36':'7','40':'9','43':'8','48':'6','50':'8', '54':'5','55':'2','60':'2','62':'7','66':'1','67':'8','82':'5','83':'6','85':'9','86':'2'}
 sudoku = []
 for i in range(9):
     row = []
+    #instantiates row
     for j in range(9):
         img = PIL.ImageOps.invert(Image.open(f"celltest row{i} col{j}.jpg"))
+        # loops through cell files saved in the_play
+        # pytesseract needs pil format, unfortunately.
         val = pytesseract.image_to_string(img, config="--psm 10 --oem 3 -c tessedit_char_whitelist=123456789")
+        # psm 10 tells tesseract to look for single characters. whitelist IS BROKEN IN TESSERACT 4
+        # Cheap whitelist replacement
         if val == '' or val not in "123456789":
             row.append(0)
+            # if cell is empty populates row with placeholder 0
         else:
             row.append(int(val))
-        # crap, interprets 1 as 4. probably border related. also one time 9 as E.
-        # interprets 9 as g on 26
+            # populates row with found number
+
     sudoku.append(row)
+    # adds row to full puzzle
 for each in sudoku:
     print(each)
--- the_play.py
+++ the_play.py
@@ -5,22 +5,30 @@ import os
 import numpy as np
 
 img = cv2.imread("sudoku_clean.png",0)
-blur = cv2.bilateralFilter(img,9,75,75) #bilateral is probably best for my purposes
+blur = cv2.bilateralFilter(img,9,75,75) #bilateral is probably best for my purposes. blurs like colored sections to reduce noise, but maintains border integrity
 
+# size grabs size of image, size//1200 determined through experimentation to give best results
+# adaptiveThreshold() used below requires sample size to be odd.
 sample = blur.size//1200
 if sample % 2 == 0:
     sample += 1
 elif sample < 11:
     sample = 11
 
+# adaptive threshold applies value based one darker/lighter {sample} nearby pixels
+# thresh_binary converts pixel from greyscale to pure black/white based on adaptive values
 blur2 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,sample ,2)
 
+# countours becomes list of all found contours (solid line shapes)
+# POTENTIAL ISSUE: current format assumes that borders will be contained inside photo. system does not function if image cuts any borders off (should be a non issue
 contoured, contours, hierarchy = cv2.findContours(blur2,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
- 
+
 area = 0
+# checks how far the lines of a contour shape warp, and if they're within epsilon boundaries, simplifies them. basically converts a badly drawn square to square
 for cnt in contours:
     epsilon = 0.1*cv2.arcLength(cnt,True)
     approx = cv2.approxPolyDP(cnt,epsilon,True)
+    # assumes sudoku puzzle is the largest single contour in the image.
     if cv2.contourArea(cnt) > area:
         area = cv2.contourArea(approx)
         puzzle = approx
@@ -29,13 +37,11 @@ pts1=np.float32(puzzle)
 pts2=np.float32([[0,0],[0,315],[315,315],[315,0]])
 m = cv2.getPerspectiveTransform(pts1,pts2)
 t = cv2.warpPerspective(blur2,m,(315,315))
-
-cv2.imshow("test",t)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# uses corners of found puzzle contour to adjust angled/distorted puzzle into corrected square
 
 for col in range(9):
     for row in range(9):
         cv2.imwrite(f'celltest row{row} col{col}.jpg', t[row*35+5:(row+1)*35-3,col*35+5:(col+1)*35-3])
 
+# iterates through all cells of puzzle and saves all cell contents with row/col index
+# per remapping conditions,each cell is 35px*35px. row*35+5:row+1*35-3 grab only contents, not borders. borders screwed up text recognition
