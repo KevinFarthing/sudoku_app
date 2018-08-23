25ac26eb2d6467ee120770e29ec9198ee7548999
Kevin <farthing.kw@gmail.com>
2018-08-23 00:56:31 -0500

    began packaging into functions

--- tess.py
+++ tess.py
@@ -6,25 +6,25 @@ import cv2
 import os
 import numpy as np
 
-sudoku = []
-for i in range(9):
-    row = []
-    #instantiates row
-    for j in range(9):
-        img = PIL.ImageOps.invert(Image.open(f"celltest row{i} col{j}.jpg"))
-        # loops through cell files saved in the_play
-        # pytesseract needs pil format, unfortunately.
-        val = pytesseract.image_to_string(img, config="--psm 10 --oem 3 -c tessedit_char_whitelist=123456789")
-        # psm 10 tells tesseract to look for single characters. whitelist IS BROKEN IN TESSERACT 4
-        # Cheap whitelist replacement
-        if val == '' or val not in "123456789":
-            row.append(0)
-            # if cell is empty populates row with placeholder 0
-        else:
-            row.append(int(val))
-            # populates row with found number
+def img_to_sudoku():
+    """
+    assumes the_play.clean_image has run
+    iterates through all 81 cells saved by clean_image and extracts text from them
+    if text is a number from 1-9, adds that to nested list puzzle
+    if not, uses 0 as a placeholder for empty
+    """
+    sudoku = []
+    for i in range(9):
+        row = []
+        for j in range(9):
+            img = PIL.ImageOps.invert(Image.open(f"cell row{i} col{j}.jpg"))
+            val = pytesseract.image_to_string(img, config="--psm 10 --oem 3 -c tessedit_char_whitelist=123456789")
+            # psm 10 tells tesseract to look for single characters. whitelist IS BROKEN IN TESSERACT 4
+            if val == '' or val not in "123456789":
+                row.append(0)
+            else:
+                row.append(int(val))
 
-    sudoku.append(row)
-    # adds row to full puzzle
-for each in sudoku:
-    print(each)
+        sudoku.append(row)
+    for each in sudoku:
+        print(each)
--- the_play.py
+++ the_play.py
@@ -5,43 +5,42 @@ import os
 import numpy as np
 
 img = cv2.imread("sudoku_clean.png",0)
-blur = cv2.bilateralFilter(img,9,75,75) #bilateral is probably best for my purposes. blurs like colored sections to reduce noise, but maintains border integrity
 
-# size grabs size of image, size//1200 determined through experimentation to give best results
-# adaptiveThreshold() used below requires sample size to be odd.
-sample = blur.size//1200
-if sample % 2 == 0:
-    sample += 1
-elif sample < 11:
-    sample = 11
+def clean_image(img):
+    """given image, uses cv2 to reduce noise using bilateral filter
+    converts to pure B/W with dynamically generated adaptive thresholding samples
+    remaps image using contour detection to find puzzle corners in image
+    splits image into 81 individual cells saved in static/images
+    ASSUMPTION:
+    photo contains space beyond borders of puzzle. if not, it inhibits contour detection
+    converts image so each cell is 35x35. assumes 5px on each side of cell contain only borders, and no relevent data.
+    """
+    blur = cv2.bilateralFilter(img,9,75,75)
 
-# adaptive threshold applies value based one darker/lighter {sample} nearby pixels
-# thresh_binary converts pixel from greyscale to pure black/white based on adaptive values
-blur2 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,sample ,2)
+    sample = blur.size//1200
+    if sample % 2 == 0:
+        sample += 1
+    elif sample < 11:
+        sample = 11
 
-# countours becomes list of all found contours (solid line shapes)
-# POTENTIAL ISSUE: current format assumes that borders will be contained inside photo. system does not function if image cuts any borders off (should be a non issue
-contoured, contours, hierarchy = cv2.findContours(blur2,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    blur2 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,sample ,2)
 
-area = 0
-# checks how far the lines of a contour shape warp, and if they're within epsilon boundaries, simplifies them. basically converts a badly drawn square to square
-for cnt in contours:
-    epsilon = 0.1*cv2.arcLength(cnt,True)
-    approx = cv2.approxPolyDP(cnt,epsilon,True)
-    # assumes sudoku puzzle is the largest single contour in the image.
-    if cv2.contourArea(cnt) > area:
-        area = cv2.contourArea(approx)
-        puzzle = approx
+    # POTENTIAL ISSUE: current format assumes that borders will be contained inside photo. system does not function if image cuts any borders off (should be a non issue
+    contoured, contours, hierarchy = cv2.findContours(blur2,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
-pts1=np.float32(puzzle)
-pts2=np.float32([[0,0],[0,315],[315,315],[315,0]])
-m = cv2.getPerspectiveTransform(pts1,pts2)
-t = cv2.warpPerspective(blur2,m,(315,315))
-# uses corners of found puzzle contour to adjust angled/distorted puzzle into corrected square
+    area = 0
+    for cnt in contours:
+        epsilon = 0.1*cv2.arcLength(cnt,True)
+        approx = cv2.approxPolyDP(cnt,epsilon,True)
+        if cv2.contourArea(cnt) > area:
+            area = cv2.contourArea(approx)
+            puzzle = approx
 
-for col in range(9):
-    for row in range(9):
-        cv2.imwrite(f'celltest row{row} col{col}.jpg', t[row*35+5:(row+1)*35-3,col*35+5:(col+1)*35-3])
+    pts1=np.float32(puzzle)
+    pts2=np.float32([[0,0],[0,315],[315,315],[315,0]])
+    m = cv2.getPerspectiveTransform(pts1,pts2)
+    t = cv2.warpPerspective(blur2,m,(315,315))
 
-# iterates through all cells of puzzle and saves all cell contents with row/col index
-# per remapping conditions,each cell is 35px*35px. row*35+5:row+1*35-3 grab only contents, not borders. borders screwed up text recognition
+    for col in range(9):
+        for row in range(9):
+            cv2.imwrite(f'cell row{row} col{col}.jpg', t[row*35+5:(row+1)*35-3,col*35+5:(col+1)*35-3])
